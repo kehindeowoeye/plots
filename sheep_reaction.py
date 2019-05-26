dbda4dcaf6b68eac21cb69ad51a8b356644f4923
owoeye kehinde <kehindeowoeye@yahoo.com>
2019-05-26 22:21:14 +0100

    Update sheep_reaction.py

--- sheep_reaction.py
+++ sheep_reaction.py
@@ -1,5 +1,12 @@
 
-
+  import math
+  import xlrd
+  import xlsxwriter
+  import pandas as pd
+  import numpy as np
+  import mixed
+  
+  
   gr = np.array(pd.read_excel('MI_sheep.xlsx',header=None))
   gr = np.delete(gr,(0),axis = 1);
   gr = np.delete(gr,(0),axis = 0);
